41a77f6825486d4996f60b982f7a49db069d881f
Ahmet Türkmen <turkmenahmettalha@gmail.com>
2022-08-06 19:27:12 +0300

    Sprite Animation

---
+++ Bomberman Clone/Assets/Scripts/AnimatedSpriteRenderer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite idleSprite;
+    public Sprite[] animationSprites;
+
+    private int animationFrame;
+    public float animationTime = 0.25f;
+
+    public bool loop = true;
+    public bool idle = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), animationTime, animationTime);
+    }
+
+    private void NextFrame()
+    {
+        animationFrame++;
+        if (loop && animationFrame >= animationSprites.Length) {
+            animationFrame = 0;
+        }
+
+        if (idle) {
+            spriteRenderer.sprite = idleSprite;
+        }
+        else if (animationFrame >= 0 && animationFrame < animationSprites.Length) {
+            spriteRenderer.sprite = animationSprites[animationFrame];
+        }
+    }
+}
--- Bomberman Clone/Assets/Scripts/MovementController.cs
+++ Bomberman Clone/Assets/Scripts/MovementController.cs
@@ -14,9 +14,16 @@ public class MovementController : MonoBehaviour
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
 
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
     }
 
     private void Update()
@@ -37,27 +44,35 @@ public class MovementController : MonoBehaviour
 
         if (Input.GetKey(inputUp))
         {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up, spriteRendererUp);
         }
         else if (Input.GetKey(inputDown))
         {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
         }
         else if (Input.GetKey(inputLeft))
         {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left, spriteRendererLeft);
         }
         else if (Input.GetKey(inputRight))
         {
-            SetDirection(Vector2.right);
+            SetDirection(Vector2.right, spriteRendererRight);
         }
         else
         {
-            SetDirection(Vector2.zero);
+            SetDirection(Vector2.zero, activeSpriteRenderer);
         }
     }
-    private void SetDirection(Vector2 newDirection)
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
     {
         direction = newDirection;
+
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
     }
 }
