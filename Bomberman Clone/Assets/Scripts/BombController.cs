b65116d6e980c952f4033050368e520f97ee0f35
Ahmet Türkmen <turkmenahmettalha@gmail.com>
2022-08-06 21:40:34 +0300

    Bomb Controller

---
+++ Bomberman Clone/Assets/Scripts/BombController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public KeyCode inputKey = KeyCode.Space;
+    public GameObject bombPrefab;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+
+    private int bombsRemaining;
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bomb")) {
+            other.isTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (bombsRemaining > 0 && Input.GetKeyDown(inputKey)) {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        Destroy(bomb);
+        bombsRemaining++;
+    }
+}
