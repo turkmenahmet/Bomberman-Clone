ed16ea848d2fcde860717d2a8d279f0ae5548de5
Ahmet Türkmen <turkmenahmettalha@gmail.com>
2022-08-07 15:24:42 +0300

    Item Pickups

--- Bomberman Clone/Assets/Scripts/BombController.cs
+++ Bomberman Clone/Assets/Scripts/BombController.cs
@@ -109,4 +109,10 @@ public class BombController : MonoBehaviour
             destructibleTiles.SetTile(cell, null);
         }
     }
+
+    public void AddBomb()
+    {
+        bombAmount++;
+        bombsRemaining++;
+    }
 }
